c116d73ba6d564a0f1d2f8dc6422c194c7ada5cc
Marat <maratfilpv@gmail.com>
2022-09-14 13:08:32 +0300

    Осталось сделать бота
    ураураура я счастлив

--- main.py
+++ main.py
@@ -1,30 +1,51 @@
 import requests
-from pprint import pprint
+# from pprint import pprint
 from config import weather_token
 
 
 def get_weather(city, weather_token):
+
+    smile = {
+        "Clear": "Ясно \U00002600",
+        "Clouds": "Облачно \U00002601",
+        "Rain": "Дождь \U00001F327",
+        "Grizzle": "Дождь \U00001F327",
+        "Thunderstorm": "Гроза \U00001F329",
+        "Snow": "Снег \U00001F328",
+        "Mist": "Туман \U00001F32B"
+    }
+
     try:
         r = requests.get(
-            f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_token}&units=metric'
+            f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_token}&units=metric&lang=ru'
         )
         data = r.json()
         # pprint(data)
 
+        weather_main = data['weather'][0]['main']
+        if weather_main in smile:
+            wm = smile[weather_main]
+        else:
+            wm = '\U00001F52E'
+
         city = data['name']
         temp = data['main']['temp']
-        weather = data['weather']['description']
-        print(f'Погода в Городе {city}°C : {temp}\nweather')
+        temp = float('{:.1f}'.format(temp))
+        humidity = data['main']['humidity']
+        wind = data['wind']['speed']
+        print(f'Погода в Городе {city}: {wm}\n'
+              f'Температура: {temp}°C\n'
+              f'Скорость Ветра: {wind} м/с\n'
+              f'Влажность: {humidity}%\n'
+              f''
+              )
 
-
-    except Exception as ex:
-        print('ex')
-        print('Check City Name!')
-
+    except Exception:
+        print('Проверьте название Города!')
 
 
 def main():
-    city = input('Please, input name of the City: ')
+    city = input('Пожалуйста, Введите Город: ')
     get_weather(city, weather_token)
 
 
